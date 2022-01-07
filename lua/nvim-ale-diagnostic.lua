2839e6979034c52cbb4e967977c802ec1cfcb39c
Ryan Fitzgerald <rwfitzge@gmail.com>
2022-01-07 11:28:36 -0800

    Actually (hackily) fix compatibility, remove older compat

--- lua/nvim-ale-diagnostic.lua
+++ lua/nvim-ale-diagnostic.lua
@@ -5,68 +5,42 @@ local ale_diagnostic_severity_map = {
   [vim.lsp.protocol.DiagnosticSeverity.Hint] = "I";
 }
 
-vim.lsp.diagnostic.original_clear = vim.lsp.diagnostic.clear
-vim.lsp.diagnostic.clear = function(bufnr, client_id, diagnostic_ns, sign_ns)
-  vim.lsp.diagnostic.original_clear(bufnr, client_id, diagnostic_ns, sign_ns)
+vim.diagnostic.original_reset = vim.diagnostic.reset
+vim.diagnostic.reset = function(namespace, bufnr)
+  vim.diagnostic.original_reset(namespace, bufnr)
+
   -- Clear ALE
   vim.api.nvim_call_function('ale#other_source#ShowResults', {bufnr, "nvim-lsp", {}})
   vim.b[bufnr].prev_nvim_lsp_diagnostics = nil
 end
 
-if vim.version().api_level >= 8 then
-  local function set_signs(bufnr)
-      -- Get all diagnostics from the current buffer
-    local diagnostics = vim.diagnostic.get(bufnr)
-    local items = {}
+function vim.diagnostic.show(namespace, bufnr, ...)
+  -- Get all diagnostics from the current buffer
+  local diagnostics = vim.diagnostic.get(bufnr)
+  local items = {}
 
-    for _, item in ipairs(diagnostics) do
-      local nr = ''
-      if item.user_data and item.user_data.lsp and item.user_data.lsp.code then
-        nr = item.user_data.lsp.code
-      end
-      table.insert(items, {
-        nr = nr,
-        text = item.message,
-        lnum = item.lnum+1,
-        end_lnum = item.end_lnum,
-        col = item.col+1,
-        end_col = item.end_col,
-        type = ale_diagnostic_severity_map[item.severity]
-      })
+  for _, item in ipairs(diagnostics) do
+    local nr = ''
+    if item.user_data and item.user_data.lsp and item.user_data.lsp.code then
+      nr = item.user_data.lsp.code
     end
-
-    -- Only call `ShowResults` if the diagnostics have actually changed
-    if not vim.deep_equal(vim.b[bufnr].prev_nvim_lsp_diagnostics, items) then
-      vim.api.nvim_call_function(
-        'ale#other_source#ShowResults',
-        {bufnr, "nvim-lsp", items}
-      )
-      vim.b[bufnr].prev_nvim_lsp_diagnostics = items
-    end
+    table.insert(items, {
+      nr = nr,
+      text = item.message,
+      lnum = item.lnum+1,
+      end_lnum = item.end_lnum,
+      col = item.col+1,
+      end_col = item.end_col,
+      type = ale_diagnostic_severity_map[item.severity]
+    })
   end
 
-  function vim.diagnostic.show(namespace, bufnr, ...)
-    set_signs(bufnr)
-  end
-else
-  vim.lsp.diagnostic.set_signs = function(diagnostics, bufnr, _, _, _)
-    if not diagnostics then
-      return
-    end
-
-    local items = {}
-    for _, item in ipairs(diagnostics) do
-      table.insert(items, {
-        nr = item.code,
-        text = item.message,
-        lnum = item.range.start.line+1,
-        end_lnum = item.range['end'].line,
-        col = item.range.start.character+1,
-        end_col = item.range['end'].character,
-        type = ale_diagnostic_severity_map[item.severity]
-      })
-    end
-
-    vim.api.nvim_call_function('ale#other_source#ShowResults', {bufnr, "nvim-lsp", items})
+  -- Only call `ShowResults` if the diagnostics have actually changed
+  if not vim.deep_equal(vim.b[bufnr].prev_nvim_lsp_diagnostics, items) then
+    vim.api.nvim_call_function(
+      'ale#other_source#ShowResults',
+      {bufnr, "nvim-lsp", items}
+    )
+    vim.b[bufnr].prev_nvim_lsp_diagnostics = items
   end
 end
